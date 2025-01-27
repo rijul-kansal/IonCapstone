709764baa723d0d06b16e9f5372e3715e0f48b24
rijul-kansal <kansalrijul@gmail.com>
2025-01-27 15:07:13 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const morgan = require('morgan');
 const AuthRouter = require('./Router/AuthRouter')
 const app = express();
@@ -9,6 +10,7 @@ const port = 3200;
 // Middleware to parse JSON
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 // MongoDB connection string
 const mongoURI = "mongodb+srv://kansalrijul:aFX2w8hC6jw2i1jC@cluster0.jbige.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose
